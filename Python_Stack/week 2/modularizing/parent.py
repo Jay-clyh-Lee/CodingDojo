31fa2452fab0f1b09a2fc86c4605de69c3a6ec72
Jay-clyh-Lee <jayclyhli@gmail.com>
2021-11-10 14:31:51 -0800

    update

---
+++ Python_Stack/week 2/modularizing/parent.py
@@ -0,0 +1,9 @@
+local_val = "magical unicorns"
+def square(x):
+    return x * x
+class User:
+    def __init__(self, name):
+        self.name = name
+    def say_hello(self):
+        return "hello"
+
