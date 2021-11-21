12bb7456d3c0fca2bc6fb4f46da228a62fa55581
Jay-clyh-Lee <jayclyhli@gmail.com>
2021-11-20 21:32:08 -0800

    great number game

--- Python_Stack/week_4/great_number_game/server.py
+++ Python_Stack/week_4/great_number_game/server.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, redirect, session, request
+import random
 
 app = Flask(__name__)
 app.secret_key="meow"
 
+
 @app.route('/')
 def index():
-    
-    return render_template('index.html')
+    if "tries" not in session:
+        session["tries"] = 0
+    if "my_number" not in session:
+        session["my_number"] = random.randint(1, 101)
+        # print(f'THE TYPE OF THIS SESSION IS :: : : : : : {type(session["my_number"])}')
+    return render_template('index.html', my_number = session["my_number"], tries = session["tries"])
 
 @app.route('/guess', methods=['POST'])
 def guess():
-    number = request.form
+    session["user_number"] = int(request.form['guess'])
+    # print(f'OMWQEWLKJS:LFJLFJGGJ ++++++++++++++++++++++++++++++ {type(request.form["guess"])}')
+    return redirect('/')
 
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 
 if __name__ == "__main__":
