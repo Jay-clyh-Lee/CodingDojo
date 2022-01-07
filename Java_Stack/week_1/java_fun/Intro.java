be7707f30c378a79dc6e749517f0a9d517eda00a
Jay-clyh-Lee <jayclyhli@gmail.com>
2022-01-07 15:27:15 -0800

    java_intro

---
+++ Java_Stack/week_1/java_fun/Intro.java
@@ -0,0 +1,9 @@
+package java_fun;
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("My name is Jay.");
+        System.out.println("I am 30+ years old.");
+        System.out.println("I was born in China and lived in California for 15 years.");
+    }
+}
