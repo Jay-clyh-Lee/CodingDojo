c9f0c3de8519d3d6a3dc85dc196097bcb5c0c792
Jay-clyh-Lee <jayclyhli@gmail.com>
2022-01-16 18:27:54 -0800

    a

--- Java_Stack/week_1/cafe_business_logic/CafeUtil.java
+++ Java_Stack/week_1/cafe_business_logic/CafeUtil.java
@@ -1,10 +1,11 @@
 package cafe_business_logic;
 import java.util.ArrayList;
+
 public class CafeUtil {
 
-    public Integer getStreakGoal(Integer numWeeks) {
+    public Integer getStreakGoal() {
         int sum = 0;
-        for (int i = 1; i < numWeeks; i++){
+        for (int i = 1; i <= 10; i++){
             sum = sum + i;
         }
         return sum;
@@ -19,13 +20,18 @@ public class CafeUtil {
     }
     public void displayMenu(ArrayList<String> menuItems) {
         for (int i = 0; i < menuItems.size(); i++) {
-            System.out.println(menuItems.get(i));
+            System.out.println(i + " " + menuItems.get(i));
         }
     }
 
-    public String greetSpanish(String name) {
-        return "Hola, " + name;
-        
+    public void addCustomer(ArrayList<String> customers) {
+        String userName = System.console().readLine();
+        customers.add(userName);
     }
+
+    public boolean displayMenu(ArrayList<String> menuItems, ArrayList<Double> prices) {
+        return menuItems.size() == prices.size();
+    }
+
 }
     
