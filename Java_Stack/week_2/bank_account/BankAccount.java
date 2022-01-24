6c6a4b25cd704b2f91b20d002659b7f3fa08c50a
Jay-clyh-Lee <jayclyhli@gmail.com>
2022-01-23 21:01:29 -0800

    init

---
+++ Java_Stack/week_2/bank_account/BankAccount.java
@@ -0,0 +1,8 @@
+public class BankAccount {
+    
+    double checkingBalance;
+    double savingsBalance;
+    public static double totalAmount;   
+    public double total = checkingBalance + savingsBalance;
+    
+}
