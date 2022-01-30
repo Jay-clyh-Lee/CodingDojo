4ac12ed913bed0f19d51eb939bbf88da80e53308
Jay-clyh-Lee <jayclyhli@gmail.com>
2022-01-30 14:19:06 -0800

    zookeeper

---
+++ Java_Stack/week_2/zoo_keeper/Mammal.java
@@ -0,0 +1,12 @@
+public class Mammal {
+    
+    public int energyLevel = 100;
+
+    public int displayEnergy() {
+        // getter
+        System.out.println("Current enery level:" + energyLevel);
+        return energyLevel;
+    }
+
+    
+}
