42f16e612c9788b98eb353031cea265b78e74e4c
Jay-clyh-Lee <jayclyhli@gmail.com>
2022-04-06 21:19:10 -0600

    deletion

---
+++ Java_Stack/week_2/coffeedore_64/Item.java
@@ -0,0 +1,31 @@
+public class Item {
+    
+        // MEMBER VARIABLES
+        private String name;
+        private double price;
+        
+        // CONSTRUCTOR
+        public Item(){}
+
+        public Item(String name, double price){
+            this.name = name;
+            this.price = price;
+        }
+        
+        // GETTERS & SETTERS  - for name and price
+        public String getName(){
+            return this.name;
+        }
+        
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public double getPrice(){
+            return this.price;
+        }
+        
+        public void setPrice(double price){
+            this.price = price;
+        }
+}
---
+++ Java_Stack/week_2/coffeedore_64/OrderKiosk.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class OrderKiosk {
+
+    // ATTRIBUTES/MEMBER VARIABLES
+    ArrayList<Item> menu = new ArrayList<Item>();
+    ArrayList<Order> orders = new ArrayList<Order>();
+
+    // CONSTRUCTOR
+    pubic OrderKiosk(){}
+
+    public void addMenuItem(String name, double price){
+        menu.add(name, price);
+    }
+
+    public 
+}
