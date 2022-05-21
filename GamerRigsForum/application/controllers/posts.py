5214daa47b84ddb560340bee3f8d5abdc2b4a1b6
Jay-clyh-Lee <jayclyhli@gmail.com>
2022-05-21 10:13:48 -0600

    get it done!

--- GamerRigsForum/application/controllers/posts.py
+++ GamerRigsForum/application/controllers/posts.py
@@ -6,33 +6,30 @@ from config.mysqlconnection import connectToMySQL
 @app.route('/posts/create', methods=['POST'])
 def create():
     data = {
-        "name" : request.form['name'],
-        "description" : request.form['description'],
-        "price": request.form["price"],
-        "quantity": request.form["quantity"],
+        "comment" : request.form['comment'],
         "user_id": session['user_id']
     }
     if not post.Post.validate_post(data):
-        return redirect('/posts/new')
-    post.Post.create_post(data)
+        return redirect('/dashboard')
+    post.Post.save(data)
     return redirect('/dashboard')
 
-@app.route('/posts/new')
-def new():
-    user_data = {
-        "id": session["user_id"]
-    }
-    return render_template("new.html", logged_in_user = user.User.get_by_id(user_data))
+# @app.route('/posts/new')
+# def new():
+#     user_data = {
+#         "id": session["user_id"]
+#     }
+#     return render_template("new.html", logged_in_user = user.User.get_by_id(user_data))
 
 @app.route('/posts/show/<int:post_id>')
 def show(post_id):
     user_data = {
         "id": session["user_id"]
     }
-    data = {                                                           
+    post_data = {                                                           
         "id": post_id,
     }
-    return render_template("show.html", logged_in_user = user.User.get_by_id(user_data), post = post.post.get_by_id(data))
+    return render_template("show.html", logged_in_user = user.User.get_by_id(user_data), gpost = post.Post.get_by_id(post_data))
 
 @app.route('/posts/edit/<int:post_id>')
 def edit(post_id):
@@ -42,7 +39,7 @@ def edit(post_id):
     data = {                                                           
         "id": post_id,
     }
-    return render_template("edit.html", logged_in_user = user.User.get_by_id(user_data), post = post.post.get_by_id(data))
+    return render_template("edit.html", logged_in_user = user.User.get_by_id(user_data), post = post.Post.get_by_id(data))
 
 @app.route('/posts/update/<int:post_id>', methods=['POST'])
 def update(post_id):
@@ -68,12 +65,10 @@ def destroy(post_id):
     return redirect('/dashboard')
 
 @app.route('/posts/<int:post_id>/like', methods=['POST'])
-def buy_post(post_id):
-    buyer_data = {  
+def like_post(post_id):
+    liker_data = {  
         "user_id": session['user_id'],
         "post_id": post_id
     }
-    query = f"UPDATE posts SET sold=sold+1 WHERE id = {post_id};"
-    connectToMySQL(db).query_db(query)
-    post.Post.buy_post(buyer_data)
-    return redirect(f'/posts/show/{post_id}')
+    post.Post.like_post(liker_data)
+    return redirect('/dashboard')
